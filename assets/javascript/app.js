3dae423f08705e91eaa2f92764705ae474f6cd3d
jsalamat <salamat_jon@yahoo.com>
2018-03-20 22:58:32 -0400

    js add class2body 2hide default and add active to show

---
+++ assets/javascript/app.js
@@ -0,0 +1,7 @@
+const cursor = document.querySelector('.cursor');
+const canvas = document.querySelector('.canvas');
+document.addEventListener('DOMContentLoaded',
+ function() {
+ 	document.body.classList.add('no-cursor');
+ 	cursor.classList.add('active');
+});
