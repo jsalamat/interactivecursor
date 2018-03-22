e5ba0a523d58c4cba23d51521c1b1c09fd267028
jsalamat <salamat_jon@yahoo.com>
2018-03-21 23:55:50 -0400

    js add notes and start listen to mouse

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,3 +1,5 @@
+// Add a class to the body to hide the default cursor
+// Add active class to show new cursor
 const cursor = document.querySelector('.cursor');
 const canvas = document.querySelector('.canvas');
 document.addEventListener('DOMContentLoaded',
@@ -5,3 +7,7 @@ document.addEventListener('DOMContentLoaded',
  	document.body.classList.add('no-cursor');
  	cursor.classList.add('active');
 });
+
+// Listen to mouse
+// - get the new cursor following and rect to moue does
+// - start listen to mouse events
